6283e5978e0f4dae312e802f212384b32a8ddd9b
Justin Welenofsky <welenofsky@gmail.com>
2016-09-14 19:37:35 -0700

    Python 2 and 3 support, tabs -> spaces

--- PyWatchdog.py
+++ PyWatchdog.py
@@ -9,32 +9,42 @@ Authored by Justin Welenofsky
 import socket
 import time
 import winsound
-from tkinter import *
+
+try:
+    # Python2
+    import Tkinter as tkinter
+except ImportError:
+    # Python3
+    import tkinter as tkinter
+
 
 #Website or IP address of outside world
 ip = 'www.google.com' 
 port = 80
-	
+
 def checkI():
-	isActive = 1
-	isSent = 0
-	while isActive == 1:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		try:
-			s.connect((ip, port))
-			if isSent == 0:
-				isSent = 1
-				print("Connected to IP", ip)
-			s.shutdown(2)
-			s.close()
-		except socket.error as e:
-			print("Internetz Down!!!1")
-			print(e)
-			#Play exclamation sound when internet goes down
-			winsound.PlaySound("SystemHand", winsound.MB_ICONHAND)
-			isActive = 0
-		time.sleep(5);
-		
+    isActive = 1
+    isSent = 0
+    while isActive == 1:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            s.connect((ip, port))
+            if isSent == 0:
+                isSent = 1
+                print("Connected to IP", ip)
+            s.shutdown(2)
+            s.close()
+        except socket.error as e:
+            print("Internetz Down!!!1")
+            print(e)
+            #Play exclamation sound when internet goes down
+            winsound.PlaySound("SystemHand", winsound.MB_ICONHAND)
+
+            app.mainloop()
+            
+            isActive = 0
+        time.sleep(5);
+        
 class Application(tkinter.Frame):
     def __init__(self, master=None):
         tkinter.Frame.__init__(self, master)
@@ -44,18 +54,18 @@ class Application(tkinter.Frame):
     def createWidgets(self):
         self.OKAY = tkinter.Button(self)
         self.OKAY["text"] = "Internet Downzzz\n(click me)"
-        #self.OKAY["command"] = self.say_hi
+        self.OKAY["command"] = self.say_hi
         self.OKAY.pack(side="top")
 
         self.QUIT = tkinter.Button(self, text="FUCK DIS", fg="red",
                                             command=root.destroy)
         self.QUIT.pack(side="bottom")
-"""
+
     def say_hi(self):
         print("Pfft...What a downer")
-"""       
+
 root = tkinter.Tk()
 app = Application(master=root)
 print("Connecting to External IP...")
 checkI()
-app.mainloop()
+
